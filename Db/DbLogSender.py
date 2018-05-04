5d61ad11c4095e3823776ea6217278c525b3fad0
martem4 <artem.moshiyan@gmail.com>
2018-05-04 17:00:39 +0300

    21 century log format parser

--- Db/DbLogSender.py
+++ Db/DbLogSender.py
@@ -1,28 +1,32 @@
 import MySQLdb
 
-class LogToDb:
+
+class DbLogSender:
     def __init__(self, dbConfig, logConfig):
         self.dbConfig = dbConfig
         self.logConfig = logConfig
         self.db = None
 
-    @staticmethod
     def connectToDb(self):
-        db = MySQLdb.connect(self.dbConfig.url,
-                             self.dbConfig.port,
-                             self.dbConfig.user,
-                             self.dbConfig.passw,
-                             self.dbConfig.dbname
-                             )
+        self.db = MySQLdb.connect(host=str(self.dbConfig.url),
+                                  user=str(self.dbConfig.user),
+                                  passwd=str(self.dbConfig.passw),
+                                  db=str(self.dbConfig.dbname),
+                                  charset='utf8'
+                                  )
 
-    @staticmethod
     def sendLog(self, line):
         query = "INSERT INTO `systemevents` (`ReceivedAt`, `DeviceReportedTime`" \
-                ", `Facility`,`Priority`,	`FromHost`, `Message`," \ 
+                ", `Facility`,`Priority`,	`FromHost`, `Message`," \
                 "`InfoUnitID`, `SysLogTag`, `EventLogType`,`GenericFileName`, `SystemID`, `processid`, " \
-                "`checksum`) VALUES (now(), now(), 9, 6, 'logserver', " \
-                "'"+ line +"', 1,'"+ self.logConfig.name +"', NULL, NULL, NULL, '', 0);"
+                "`checksum`) VALUES (now(), now(), 2, 3, 'logserver', " \
+                "'" + line + "', 1,'" + self.logConfig.name + "', NULL, NULL, NULL, '', 0);"
+        print(query)
 
+        if self.db is not None:
+            cursor = self.db.cursor()
+            cursor.execute(query)
 
-
-
+    def closeDb(self):
+        if self.db is not None:
+            self.db.close()
